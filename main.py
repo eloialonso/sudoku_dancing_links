bd7fb5ab9b45339efa8fd047c4d943b10ca3c0b2
Eloi Alonso <eloi.alonso.7.4@gmail.com>
2023-10-14 10:05:12 +0200

    Load grid in main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import argparse
+from pathlib import Path
 
 from src import solve_sudoku
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', help='Path to sudoku text file.')
+    parser.add_argument('filename', type=Path, help='Path to sudoku text file.')
+
     args = parser.parse_args()
-    solve_sudoku(path=args.filename)
+
+    with args.filename.open('r') as f:
+        grid = [list(map(int, x.strip().split(' '))) for x in f.readlines()]
+
+    solve_sudoku(grid)
 
 
 if __name__ == "__main__":
